11b7b00e3b84d17f4ee0171480923df791a1be93
Daniil <name.game2012@yandex.ru>
2025-11-29 20:23:58 +0300

    Added fraagment and vertex shader

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,40 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 
+GLfloat point[] = {
+    0.0f,  0.5f, 0.0f,
+    0.5f, -0.5f, 0.0f,
+   -0.5f, -0.5f, 0.0f
+};
+
+
+GLfloat colors[] = {
+    1.0f, 0.0f, 0.0,
+    0.0f, 1.0f, 0.0,
+    0.0f, 0.0f, 1.0f
+};
+
+
+//вершинный шейдер
+const char* vertex_shader =
+"#version 460\n"
+"layout(location = 0) in vec3 vertex_position;"//прив€зка вершины к первому атрибута модели (позици€).
+"layout(location = 1) in vec3 vertex_color;"   //прив€зка цвета вершины ко второму атрибуту модели.
+"out vec3 color;"                               //выходна€ переменна€, передающа€ данные фрагменту (цвет вершины).
+"void main() {"
+"   color = vertex_color;"
+"   gl_Position = vec4(vertex_position,1.0);"
+"}";
+
+//фрагментный шейдер
+const char* fragment_shader =
+"#version 460\n"
+"in vec3 color;"       //принимает цвет от вершинного шейдера.
+"out vec4 frag_color;" //итогова€ переменна€ цвета каждого пиксел€.
+"void main() {"
+"   frag_color = vec4(color, 1.0);"
+"}";
+
 int g_windowSizeX = 640;
 int g_windowSizeY = 480;
 
@@ -42,7 +76,7 @@ int main(void)
 
     glfwSetWindowSizeCallback(pWindow, glfwWindowSizeCallback);//эвент(коллбек) дл€ раст€гивании окна
 
-    glfwSetKeyCallback(pWindow, glfwkeyCallback);//эвент(коллбек) при нажатии на клавишу Esc выход из окна и закрытие программы
+    glfwSetKeyCallback(pWindow, glfwkeyCallback);              //эвент(коллбек) при нажатии на клавишу Esc выход из окна и закрытие программы
 
     /* Make the window's context current */
     glfwMakeContextCurrent(pWindow);
@@ -58,11 +92,56 @@ int main(void)
 
     glClearColor(1, 1, 0, 1);
 
+    GLuint vs = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vs, 1, &vertex_shader, nullptr);
+    glCompileShader(vs);
+
+
+    GLuint fs = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fs, 1, &fragment_shader, nullptr);
+    glCompileShader(fs);
+
+    GLuint shader_program = glCreateProgram();
+    glAttachShader(shader_program, vs);
+    glAttachShader(shader_program, fs);
+    glLinkProgram(shader_program);
+
+    glDeleteShader(vs);
+    glDeleteShader(fs);
+
+    GLuint points_vbo = 0;
+    glGenBuffers(1, &points_vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, points_vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(point), point, GL_STATIC_DRAW); //передаЄм в пам€ть видеокарты
+
+    GLuint colors_vbo = 0;
+    glGenBuffers(1, &colors_vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, colors_vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(colors), colors, GL_STATIC_DRAW);//передаЄм в пам€ть видеокарты
+
+
+    GLuint vao = 0;
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
+
+    glEnableVertexAttribArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, points_vbo);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
+
+    glEnableVertexAttribArray(1);
+    glBindBuffer(GL_ARRAY_BUFFER, colors_vbo);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
+
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(pWindow))
     {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
+
+        //рисовать будем тут
+        glUseProgram(shader_program);
+        glBindVertexArray(vao);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
         /* Swap front and back buffers */
         glfwSwapBuffers(pWindow);
