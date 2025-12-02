ae1a7084d6ddf06eaee009b7dbce225e9d501de3
Daniil <name.game2012@yandex.ru>
2025-12-02 19:18:14 +0300

    Added Texture that's not all

---
+++ src/Renderer/Texture2D.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <glad/glad.h>
+#include <string>
+
+namespace Renderer {
+	class Texture2D {
+	public:
+		Texture2D(const GLuint width, 
+				  const GLuint height, 
+			      const unsigned char* data, 
+				  const unsigned char channels = 4, 
+				  const GLenum filter = GL_LINEAR, 
+				  const GLenum wrapMode = GL_CLAMP_TO_EDGE);
+
+		Texture2D () = delete;
+		Texture2D (const Texture2D&) = delete;
+		Texture2D& operator =(const Texture2D&) = delete;
+		Texture2D& operator = (Texture2D && texture2d) noexcept;
+		Texture2D(Texture2D&& texture2d ) noexcept;
+
+		void bind() const;
+
+		~Texture2D();
+
+	private:
+		GLuint m_ID;
+		GLenum m_mode;
+		unsigned int m_width{ 0 };
+		unsigned int m_height{ 0 };
+
+	};
+}
--- src/Resources/ResourceManager.cpp
+++ src/Resources/ResourceManager.cpp
@@ -4,7 +4,13 @@
 #include <iostream>
 
 #include "ResourceManager.h"
+#include "../Renderer/Texture2D.h"
 #include "../../src/Renderer/ShaderProgram.h"
+
+
+#define STB_IMAGE_IMPLEMENTATION
+#define STBI_ONLY_PNG
+#include "stb_image.h"
 
 ResourceManager::ResourceManager(const std::string& executablePath) {
 	size_t found = executablePath.find_last_of("/\\");
@@ -63,6 +69,43 @@ std::shared_ptr<Renderer::ShaderProgram> ResourceManager::getShaderProgram(const
 	{
 		return it->second;
 	}
-	std::cerr << "Can't find ther shader: " << shaderName << std::endl;
+	std::cerr << "Can't find the shader: " << shaderName << std::endl;
+	return nullptr;
+}
+
+
+std::shared_ptr<Renderer::Texture2D> ResourceManager::loadTexture(const std::string& textureName, const std::string& texturePath)
+{
+	int channels{ 0 };
+	int width{ 0 };
+	int height{ 0 };
+	stbi_set_flip_vertically_on_load(true);//из-за того, что опенгл считывается снизу слева, а нам нужно сверху, поэтому мы переворачиваем картинку
+	unsigned char* pixels = stbi_load(std::string(m_path + "/" + texturePath).c_str(), &width, &height, &channels, 0);
+
+	if (!pixels)
+	{
+		std::cerr << "Can't load image: " << texturePath << std::endl;
+		return nullptr;
+	}
+
+	std::shared_ptr<Renderer::Texture2D> newTexture = m_textures.emplace(textureName, std::make_shared<Renderer::Texture2D>(width, 
+																															height,
+																															pixels,
+																															channels, 
+																															GL_NEAREST, 
+																															GL_CLAMP_TO_EDGE)).first->second;
+	stbi_image_free(pixels);
+
+	return newTexture;
+}
+
+std::shared_ptr<Renderer::Texture2D> ResourceManager::getTexture(const std::string& textureName)
+{
+	TexturesMap::const_iterator it = m_textures.find(textureName);
+	if (it != m_textures.end())
+	{
+		return it->second;
+	}
+	std::cerr << "Can't find the texture: " << textureName << std::endl;
 	return nullptr;
 }
